46e565e5f132d6dcd069b109d1be71ee4d44489f
walkonmars36 <mark@walkonmars.co.uk>
2021-07-28 19:59:11 +0100

    finished JS

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const grid = document.querySelector(".grid");
 const startBtn = document.getElementById("start");
-const score = document.getElementById("score");
+const scoreDisplay = document.getElementById("score");
 let squaresArr = [];
 let currentSnake = [2, 1, 0]; // position in the squaresArr
 let direction = 1;
 const width = 10;
 let appleIndex = 0;
+let score = 0;
+let intervalTime = 1000;
+let speed = 0.9;
+let timerID = 0;
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -24,6 +28,23 @@ function createGrid() {
 createGrid();
 
 currentSnake.forEach((element) => squaresArr[element].classList.add("snake"));
+
+function startGame() {
+  //remove the existing snake
+  currentSnake.forEach((element) => squaresArr[element].classList.remove("snake"));
+  //remove the apple
+  squaresArr[appleIndex].classList.remove("apple");
+  clearInterval(timerID);
+  currentSnake = [2, 1, 0];
+  direction = 1;
+  score = 0;
+  scoreDisplay.textContent = score;
+  intervalTime = 1000;
+  generateApple();
+  //re-add class of snake to new currentSnake
+  currentSnake.forEach((element) => squaresArr[element].classList.add("snake"));
+  timerID = setInterval(moveSnake, intervalTime);
+}
 
 function moveSnake() {
   if (
@@ -48,11 +69,20 @@ function moveSnake() {
     //remove the .apple class
     squaresArr[currentSnake[0]].classList.remove("apple");
     //grow snake by adding .snake class
-    squaresArr[currentSnake[tail]].classList.add("snake");
+    squaresArr[tail].classList.add("snake");
     //grow snake array
+    currentSnake.push(tail);
+
     //generate a new apple
+    generateApple();
     //add one to the score
+    score++;
+    //display the score
+    scoreDisplay.textContent = score;
     //speed up the snake
+    clearInterval(timerID);
+    intervalTime = intervalTime * speed;
+    timerID = setInterval(moveSnake, intervalTime);
   }
 
   //add .snake styling to new square
@@ -61,17 +91,16 @@ function moveSnake() {
 
 moveSnake();
 
-//set interval method calls the function moveSnake and repeats this in whatever time interval is set (in ms)
-let timerID = setInterval(moveSnake, 1000);
+//set interval method calls the function moveSnake and repeats this in whatever time interval is set (in ms) intervalTime default = 1000
 
-function generateApples() {
+function generateApple() {
   do {
     //generate random number
     appleIndex = Math.floor(Math.random() * squaresArr.length);
   } while (squaresArr[appleIndex].classList.contains("snake"));
   squaresArr[appleIndex].classList.add("apple");
 }
-generateApples();
+generateApple();
 
 function controlSnake(e) {
   switch (e.key) {
@@ -99,3 +128,4 @@ function controlSnake(e) {
 }
 
 document.addEventListener("keydown", controlSnake);
+startBtn.addEventListener("click", startGame);
